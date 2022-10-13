9c5c634cef8ee41c96204f8e0e9cd5219e7f9c13
AnibalPrieto <114059038+AnibalPrieto@users.noreply.github.com>
2022-10-13 14:13:43 +0200

    ejer

---
+++ manejoDeArchivos/src/principal/PPal.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package principal;
+
+import manejodearchivos.ManejoDeArchivos;
+
+/**
+ *
+ * @author Alumno Mañana
+ */
+public class PPal {
+    public static void main(String[] args) {
+        String nombreArchivo="prueba.txt";
+        ManejoDeArchivos.crearArchivo(nombreArchivo);
+        ManejoDeArchivos.escribirArchivo(nombreArchivo,"klk manin");
+        ManejoDeArchivos.agregarArchivo(nombreArchivo," otra vez por aqui");
+        ManejoDeArchivos.leerArchivo(nombreArchivo);
+        ManejoDeArchivos.leerArchivoPorPalabra(nombreArchivo);
+
+    }
+    
+}
